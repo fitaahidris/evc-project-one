bc8e9dbc8a8f984f5bced239a22cf43a89b367e0
fitaahidris <fitaahidrishassan>
2024-10-08 10:33:46 +0300

    evc project one

---
+++ evs2.py
@@ -0,0 +1,30 @@
+pin = int(input('fadalan gali pin kaaga'))
+mypin = 1020
+lacagta = 250
+number = 613083622
+if pin == mypin:      
+    print('1. itus haragaaga'  )
+    print('2. kaarka hadalka')
+    print('3. bixi biil')
+    print('4. U wareeji EVCPlus')
+    print('5. warbixin kooban')
+    print('6. salaam bank')
+    print('7. maareynta')
+    print('8. biil payment')
+else:
+    print('<-EVCPlus-> nambarka sirta ah waa khalad')       
+dooro = int(input('' ))
+if dooro == 1:
+    print('hararagaaga waa $',lacagta )
+elif dooro ==  2:
+    print('1. ku shubo ku hadal')   
+    print('2. ugu shub ku hadal')
+    kaar = int(input('' ))
+    if kaar == 1:
+          lacag = int(input('fadlam geli lacagta' ))     
+
+ 
+
+
+else:
+    print('fadlan dooro nambar saxan')    
